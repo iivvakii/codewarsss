2989ac91dcacf93faa448104ec69e199c6955a0b
iivvakii <ivan.stefanyuk.2004.m@gmail.com>
2024-07-12 13:59:07 +0300

    Two of One

---
+++ src/twoOfOne.java
@@ -0,0 +1,14 @@
+public class twoOfOne {
+    public static String longest (String s1, String s2) {
+        String res = "";
+        s1 += s2;
+        char[] arr = s1.toCharArray();
+        Arrays.sort(arr);
+        s1 = new String(arr);
+        while(s1.length() != 0){
+            res += s1.charAt(0);
+            s1 = s1.replaceAll(String.valueOf(s1.charAt(0)), "");
+        }
+        return res;
+    }
+}
