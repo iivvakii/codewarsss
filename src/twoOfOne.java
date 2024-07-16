c626f673382c220b99275c9df23869f981dad36b
iivvakii <ivan.stefanyuk.2004.m@gmail.com>
2024-07-16 12:19:41 +0300

    Two of One

--- src/twoOfOne.java
+++ src/twoOfOne.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static java.util.Arrays.*;
+
 public class twoOfOne {
     public static String longest (String s1, String s2) {
         String res = "";
         s1 += s2;
         char[] arr = s1.toCharArray();
-        Arrays.sort(arr);
+        sort(arr);
         s1 = new String(arr);
         while(s1.length() != 0){
             res += s1.charAt(0);
